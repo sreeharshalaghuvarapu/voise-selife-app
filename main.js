c25bbaa363348333a7f4757885d1c05701527937
sreeharshalaghuvarapu <72685506+sreeharshalaghuvarapu@users.noreply.github.com>
2021-02-25 18:53:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+var SpeechRecognition=window.webkitSpeechRecognition
+
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function run (event){
+
+    console.log(event);
+
+    var content = event.results[0][0].transcript;
+
+    document.getElementById("textbox").innerHTML = content;
+    console.log (content);
+
+}
+function speak(){
+    var synth = window.speechSynthesis;
+
+    speak_data = document.getElementById("textbox").value
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+}
+
+Webcam.set({
+width:360,
+hight:250,
+image_format : 'png',
+png_quality:90
+});
+camera = document.getElementById("camera");
